fd0514de879aaa7eee810f4bf31ba0d89749be10
Simone <simonepapa24@gmail.com>
2023-02-14 11:42:24 +0100

    added code to create tables on project start up

--- backend/config/db.js
+++ backend/config/db.js
@@ -12,4 +12,115 @@ const pool = mysql.createPool({
   queueLimit: 0,
 })
 
+pool.execute(
+  `CREATE TABLE IF NOT EXISTS users (
+    id            INT PRIMARY KEY auto_increment,
+    name			    VARCHAR(255) NOT NULL,
+    email			    VARCHAR(320) NOT NULL,
+    password		  CHAR(60) NOT NULL,
+    profileImage  VARCHAR(512),
+    createdAt	    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updatedAt	    DATETIME on UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+  )`
+)
+
+pool.execute(
+  `CREATE TABLE IF NOT EXISTS teams (
+    id            INT PRIMARY KEY auto_increment,
+    name          VARCHAR(255) NOT NULL,
+    createdAt	    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+    updatedAt	    DATETIME on UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+  )`
+)
+
+pool.execute(
+  `CREATE TABLE IF NOT EXISTS projects (
+    id            INT PRIMARY KEY auto_increment,
+    name          VARCHAR(255) NOT NULL,
+    description   VARCHAR(512),
+    status        ENUM('On track', 'Late', 'Off track') NOT NULL DEFAULT 'On track',
+    dueDate       DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    createdAt	    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+    updatedAt	    DATETIME on UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+  )`
+)
+
+pool.execute(
+  `CREATE TABLE IF NOT EXISTS tasks (
+    id            INT PRIMARY KEY auto_increment,
+    name          VARCHAR(255) NOT NULL,
+    priority      ENUM('High', 'Medium', 'Low', 'Unassigned') NOT NULL DEFAULT 'Unassigned',
+    status        ENUM('Completed', 'Not completed') NOT NULL DEFAULT 'Completed',
+    dueDate       DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    isSubtask     BOOLEAN NOT NULL DEFAULT false,
+    mainTask      INT,
+    createdAt	    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+    updatedAt	    DATETIME on UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    completedAt	  DATETIME on UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (mainTask) REFERENCES tasks(id)
+  )`
+)
+
+pool.execute(
+  `CREATE TABLE IF NOT EXISTS users_teams (
+    userId        INT NOT NULL,
+    teamId        INT NOT NULL,
+    role          ENUM('Team leader', 'Co-leader', 'Collaborator'),
+    createdAt	    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+    updatedAt	    DATETIME on UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (userId, teamId),
+    FOREIGN KEY (userId) REFERENCES users(id),
+    FOREIGN KEY (teamId) REFERENCES teams(id)
+  )`
+)
+
+pool.execute(
+  `CREATE TABLE IF NOT EXISTS users_projects (
+    userId        INT NOT NULL,
+    projectId     INT NOT NULL,
+    role          ENUM('Project leader', 'Co-leader', 'Collaborator'),
+    createdAt	    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+    updatedAt	    DATETIME on UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (userId, projectId),
+    FOREIGN KEY (userId) REFERENCES users(id),
+    FOREIGN KEY (projectId) REFERENCES projects(id)
+  )`
+)
+
+pool.execute(
+  `CREATE TABLE IF NOT EXISTS teams_projects (
+    teamId        INT NOT NULL,
+    projectId     INT NOT NULL,
+    createdAt	    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+    updatedAt	    DATETIME on UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (teamId, projectId),
+    FOREIGN KEY (teamId) REFERENCES teams(id),
+    FOREIGN KEY (projectId) REFERENCES projects(id)
+  )`
+)
+
+pool.execute(
+  `CREATE TABLE IF NOT EXISTS users_tasks (
+    userId        INT NOT NULL,
+    taskId        INT NOT NULL,
+    createdAt	    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+    updatedAt	    DATETIME on UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (userId, taskId),
+    FOREIGN KEY (userId) REFERENCES users(id),
+    FOREIGN KEY (taskId) REFERENCES tasks(id)
+  )`
+)
+
+pool.execute(
+  `CREATE TABLE IF NOT EXISTS projects_tasks (
+    projectId     INT NOT NULL,
+    taskId        INT NOT NULL,
+    createdAt	    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+    updatedAt	    DATETIME on UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (projectId, taskId),
+    FOREIGN KEY (projectId) REFERENCES projects(id),
+    FOREIGN KEY (taskId) REFERENCES tasks(id)
+  )`
+)
+
 module.exports = pool
