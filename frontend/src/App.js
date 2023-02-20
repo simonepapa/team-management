5405d3540e8aa03e1e71d3c65972f8986dc7885c
Simone <simonepapa24@gmail.com>
2023-02-20 09:08:47 +0100

    created responsive navigation component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
+import Navigation from "./components/Navigation"
 import PrivateRoute from "./components/PrivateRoute"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className="bg-base-300 h-screen">
       <Router>
+        <Navigation />
         <div className="bg-base-300 px-4 xl:px-0">
           <Routes>
             <Route path="/" element={<PrivateRoute />}>
---
+++ frontend/src/components/Navigation.jsx
@@ -0,0 +1,164 @@
+import { useEffect, useState } from "react"
+import { NavLink, useNavigate } from "react-router-dom"
+import {
+  BsHouseDoor,
+  BsCheck2Circle,
+  BsFileEarmarkMedical,
+  BsPeople,
+  BsBoxArrowLeft,
+} from "react-icons/bs"
+
+function Navigation() {
+  const [checkbox, setCheckbox] = useState(true)
+
+  const navigate = useNavigate()
+
+  const user = JSON.parse(localStorage.getItem("user"))
+
+  useEffect(() => {
+    document.body.classList.remove("overflow-y-hidden")
+    document.documentElement.classList.remove("overflow-y-hidden")
+    document.getElementById("sidebar").classList.remove("sidebar-wide")
+    const checkbox = document.getElementById("burger")
+    checkbox.checked = !checkbox
+  }, [navigate])
+
+  const handleMenu = () => {
+    document.body.classList.toggle("overflow-y-hidden")
+    document.documentElement.classList.toggle("overflow-y-hidden")
+    document.getElementById("sidebar").classList.toggle("sidebar-wide")
+    setCheckbox(!checkbox)
+  }
+
+  const onLogout = () => {
+    localStorage.removeItem("user")
+    navigate("/login")
+  }
+
+  return (
+    <>
+      <header className="bg-base-100 w-screen fixed z-20">
+        <div className="flex justify-between items-center px-4 xl:pl-0 py-4 w-screen xl:w-9/12 xl:mx-auto">
+          <div className="logo text-xl xl:text-3xl">Team Management</div>
+          <div className="hidden xl:flex items-center">
+            {!user && (
+              <>
+                <NavLink
+                  to="/login"
+                  className={({ isActive }) =>
+                    isActive ? "font-bold text-xl mr-12" : "text-xl mr-12"
+                  }
+                >
+                  Log in
+                </NavLink>
+                <NavLink
+                  to="/register"
+                  className={({ isActive }) =>
+                    isActive ? "font-bold text-xl mr-12" : "text-xl mr-12"
+                  }
+                >
+                  Sign up
+                </NavLink>
+              </>
+            )}
+          </div>
+          <label className="btn btn-circle btn-ghost swap swap-rotate xl:hidden burger">
+            <input type="checkbox" id="burger" />
+            <svg
+              onClick={() => handleMenu()}
+              className="w-6 h-6 swap-off fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+            </svg>
+            <svg
+              onClick={() => handleMenu()}
+              className="w-6 h-6 swap-on fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
+            </svg>
+          </label>
+        </div>
+      </header>
+      <div
+        className="sidebar bg-base-100 flex flex-col items-center xl:items-stretch z-10"
+        id="sidebar"
+      >
+        {user && (
+          <>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive
+                  ? "link-inactive sidebar-active flex items-center p-2 mx-3"
+                  : "flex items-center p-2 mx-3"
+              }
+              style={{ marginTop: "5rem" }}
+            >
+              <div className="flex items-center">
+                <BsHouseDoor className="nav-icon" />
+                <p className="ml-2">Home</p>
+              </div>
+            </NavLink>
+            <NavLink
+              to="/tasks"
+              className={({ isActive }) =>
+                isActive
+                  ? "link-inactive sidebar-active flex items-center mt-10 p-2 mx-3"
+                  : "flex items-center mt-10 p-2 mx-3"
+              }
+            >
+              <div className="flex items-center">
+                <BsCheck2Circle className="nav-icon" />
+                <p className="ml-2">Tasks</p>
+              </div>
+            </NavLink>
+            <NavLink
+              to="/projects"
+              className={({ isActive }) =>
+                isActive
+                  ? "link-inactive sidebar-active flex items-center mt-10 p-2 mx-3"
+                  : "flex items-center mt-10 p-2 mx-3"
+              }
+            >
+              <div className="flex items-center">
+                <BsFileEarmarkMedical className="nav-icon" />
+                <p className="ml-2">Projects</p>
+              </div>
+            </NavLink>
+            <NavLink
+              to="/teams"
+              className={({ isActive }) =>
+                isActive
+                  ? "link-inactive sidebar-active flex items-center mt-10 p-2 mx-3"
+                  : "flex items-center mt-10 p-2 mx-3"
+              }
+            >
+              <div className="flex items-center">
+                <BsPeople className="nav-icon" />
+                <p className="ml-2">Teams</p>
+              </div>
+            </NavLink>
+          </>
+        )}
+
+        {user && (
+          <div
+            onClick={onLogout}
+            className="flex items-center p-2 mx-3 hover:cursor-pointer mt-auto mb-10"
+          >
+            <div className="flex items-center">
+              <BsBoxArrowLeft className="nav-icon" />
+              <p className="ml-2">Logout</p>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  )
+}
+
+export default Navigation
