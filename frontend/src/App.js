642cef46733a62c8bb04d90e81456e472fc02741
Simone <simonepapa24@gmail.com>
2023-02-20 08:59:45 +0100

    home is now a private route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
+import PrivateRoute from "./components/PrivateRoute"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 import Home from "./pages/Home"
@@ -11,7 +12,9 @@ function App() {
       <Router>
         <div className="bg-base-300 px-4 xl:px-0">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<PrivateRoute />}>
+              <Route path="/" element={<Home />} />
+            </Route>
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
           </Routes>
