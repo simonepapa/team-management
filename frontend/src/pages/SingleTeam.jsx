f792dccdb961031dc8fef2a59836f1050ae77564
Simone <simonepapa24@gmail.com>
2023-02-27 10:55:50 +0100

    created basic UI

--- frontend/src/pages/SingleTeam.jsx
+++ frontend/src/pages/SingleTeam.jsx
@@ -4,14 +4,19 @@ import Modal from "react-modal"
 import { toast } from "react-toastify"
 import { BsPlusCircle, BsPencil } from "react-icons/bs"
 import Spinner from "../components/Spinner"
-import {
-  useGetTeamQuery,
-} from "../features/api/apiSlice"
+import { useGetTeamQuery } from "../features/api/apiSlice"
 
 function SingleTeam() {
   const params = useParams()
 
-  const { data: team = [], isLoading, isError, message } = useGetTeamQuery(params.teamId)
+  const {
+    data: teamObject = [],
+    isLoading,
+    isError,
+    message,
+  } = useGetTeamQuery(params.teamId)
+
+  const { team, projects, users: members } = teamObject
 
   useEffect(() => {
     if (isError) {
@@ -27,9 +32,61 @@ function SingleTeam() {
     )
   }
 
+  console.log(members)
+
   return (
     <>
-
+      <main className="container flex flex-wrap pb-4 pt-24">
+        <div className="text-xl breadcrumbs pb-6 grow w-screen truncate">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/teams">Teams</Link>
+            </li>
+            <li>
+              <p>{team.name}</p>
+            </li>
+          </ul>
+        </div>
+        <section
+          className="shadow-common h-fit bg-base-100 rounded-lg p-6"
+          style={{ width: "90vw" }}
+        >
+          <div className="flex items-center">
+            <div className="flex items-center w-full">
+              <img
+                src="https://picsum.photos/200/300"
+                alt="project thumbnail"
+                className="max-w-8 w-full h-8 rounded-lg"
+              />
+              <h2 className="text-2xl text-base-content uppercase font-bold ml-4 mr-3 max-w-75p">
+                {team.name}
+              </h2>
+              <BsPencil className="max-w-8 w-full h-8 hover:cursor-pointer" />
+            </div>
+          </div>
+          <div className="flex flex-col mt-4">
+            <h3 className="text-xl font-bold mb-4">Teammates</h3>
+            <div className="flex flex-wrap hover:cursor-pointer">
+              <div
+                className="flex items-center mr-6 mb-2 w-36"
+              >
+                <div className="flex items-center justify-center bg-base-300 rounded-full w-9 h-9 mr-1">
+                  <BsPlusCircle className="w-7 h-7" />
+                </div>
+                <div className="flex flex-col">
+                  <p className="text-normal font-bold">New member</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col mt-4">
+            <h3 className="text-xl font-bold mb-4">Projects</h3>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
